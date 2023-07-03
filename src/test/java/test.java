2b1e7a3163198104b1fafacb94a6659cd6614aea
abin <abin@xu.com>
2023-07-03 10:38:25 +0800

    third commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -9,22 +9,5 @@ import java.util.ArrayList;
 
 @SpringBootTest(classes = abin.class)
 public class test {
-    @Autowired
-    private EmployeeService employeeService;
-    @Test
-    public void add(){
-        ArrayList<Employee> employees = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            Employee employee = new Employee();
-            employee.setName("jnb" + i);
-            employee.setUsername("yui"+i);
-            employee.setPassword("123456");
-            employee.setIdNumber("134567676878767655");
-            employee.setSex("男");
-            employee.setPhone("13443876546");
-            employees.add(employee);
-        }
-        employeeService.saveBatch(employees);
-    }
 
 }
